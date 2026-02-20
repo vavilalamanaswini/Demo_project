a160a9878d6ff0ac72d6fd1672a5279231ecab92
Manu <Manu@gmail.com>
2026-02-20 12:10:15 +0530

    after commit last

--- tests/News_Reviews_Media.spec.ts
+++ tests/News_Reviews_Media.spec.ts
@@ -4,6 +4,7 @@ import { NewsPage } from "../pages/NewsPage";
 test.describe("News, Reviews & Media", () => {
 
         test("Access latest car news articles @15", async ({ page }) => {
+                test.slow();
                 const newsPage = new NewsPage(page);
                 await page.goto("/");
                 await newsPage.openNewsAndTopStories();
